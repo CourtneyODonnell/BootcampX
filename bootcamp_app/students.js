3ba8ccf29742af640c35b764e366212cf912bb5f
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-08-15 21:57:46 -0300

    prevent sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,22 +1,28 @@
 const { Pool } = require('pg');
-const args = process.argv.slice(2);
-const cohort = args[0];
-const max = args[1];
 
 const pool = new Pool({
   user: 'vagrant',
-  password: 'vagrant', 
+  password: '123', 
   host: 'localhost',
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+//store potentially malicious sql injections into an array
+const values = [`%${cohortName}`, limit];
+
+//query string
+const queryString = `
+  SELECT DISTINCT students.id as student_id, student.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+
+//pool query
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
